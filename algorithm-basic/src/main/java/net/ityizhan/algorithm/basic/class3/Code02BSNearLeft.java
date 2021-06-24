ba981053f259de1fbe000e96b14b73c7589d8a0a
linjunhao <kaituozhesh@hotmail.com>
2021-06-24 13:21:19 +0800

    modify:-代码迁移

--- algorithm-basic/src/main/java/net/ityizhan/algorithm/basic/class3/Code02BSNearLeft.java
+++ algorithm-basic/src/main/java/net/ityizhan/algorithm/basic/class3/Code02BSNearLeft.java
@@ -35,29 +35,5 @@ public class Code02BSNearLeft {
         return ans;
     }
 
-    /**
-     * 在arr上找满足<=value最右下标
-     *
-     * @param arr
-     * @param value
-     * @return
-     */
-    public static int rightNearestIndex(int[] arr, int value) {
-        if (arr == null || arr.length == 0) {
-            return -1;
-        }
-        int left = 0;
-        int right = arr.length - 1;
-        int ans = -1;
-        while (left <= right) {
-            int mid = (right - left) / 2 + left;
-            if (arr[mid] <= value) {
-                ans = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return ans;
-    }
+
 }

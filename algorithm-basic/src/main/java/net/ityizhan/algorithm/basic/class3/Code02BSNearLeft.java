d6e33961c3090c887fafa394aade072ffa61762c
linjunhao <kaituozhesh@hotmail.com>
2021-06-24 11:13:12 +0800

    add:-寻找有序数组中>=value最左的位置 add:-寻找有序数组中<=value最右的位置

---
+++ algorithm-basic/src/main/java/net/ityizhan/algorithm/basic/class3/Code02BSNearLeft.java
@@ -0,0 +1,63 @@
+package net.ityizhan.algorithm.basic.class3;
+
+/**
+ * @ClassName Code02BSNearLeft
+ * @Description
+ * @Author 骚豪 程序驿站: https://www.ityizhan.net
+ * @Date 2021/6/24 10:55
+ * @Version V1.0.0
+ **/
+public class Code02BSNearLeft {
+
+    /**
+     * 在arr上找满足>=value的最左下标
+     *
+     * @param arr
+     * @param value
+     * @return
+     */
+    public static int leftNearestIndex(int[] arr, int value) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = arr.length - 1;
+        int ans = -1;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            if (arr[mid] >= value) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 在arr上找满足<=value最右下标
+     *
+     * @param arr
+     * @param value
+     * @return
+     */
+    public static int rightNearestIndex(int[] arr, int value) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = arr.length - 1;
+        int ans = -1;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            if (arr[mid] <= value) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+}

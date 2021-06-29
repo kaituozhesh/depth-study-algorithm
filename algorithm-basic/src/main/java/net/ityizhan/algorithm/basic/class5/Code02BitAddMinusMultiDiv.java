41e0078d5dbc5d84201a93ba8a460f2844338c5d
linjunhao <kaituozhesh@hotmail.com>
2021-06-29 17:43:44 +0800

    add:-位运算实现加 减 乘

---
+++ algorithm-basic/src/main/java/net/ityizhan/algorithm/basic/class5/Code02BitAddMinusMultiDiv.java
@@ -0,0 +1,53 @@
+package net.ityizhan.algorithm.basic.class5;
+
+/**
+ * @ClassName Code02BitAddMinusMultiDiv
+ * @Description
+ * @Author 骚豪 程序驿站: https://www.ityizhan.net
+ * @Date 2021/6/29 17:04
+ * @Version V1.0.0
+ **/
+public class Code02BitAddMinusMultiDiv {
+
+    public static int add(int a, int b) {
+        int sum = a;
+        // 直到进位信息没有了  就是结果了
+        while (b != 0) {
+            // 获取a + b 无进位相加信息
+            sum = a ^ b;
+            // 获取a + b 的进位信息
+            b = (a & b) << 1;
+            a = sum;
+        }
+        return sum;
+    }
+
+    public static int negNum(int a) {
+        return add(~a, 1);
+    }
+
+    public static int minus(int a, int b) {
+        // a - b = a + b的相反数  负数的二进制相反数位 负数取反加1
+        return add(a, negNum(b));
+    }
+
+    public static int multi(int a, int b) {
+        int res = 0;
+        while (b != 0) {
+            if ((b & 1) != 0) {
+                res = add(res, a);
+            }
+            a <<= 1;
+            b >>>= 1;
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(add(1, 5));
+        System.out.println(minus(7, 5));
+        System.out.println(multi(2, 5));
+        System.out.println(multi(2, -5));
+    }
+}

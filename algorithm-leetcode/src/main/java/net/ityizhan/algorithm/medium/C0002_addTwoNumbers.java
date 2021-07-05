f83fd81832546dabc785062028120ee73b86c172
linjunhao <kaituozhesh@hotmail.com>
2021-07-05 10:26:47 +0800

    add:-LeetCode:0002题:两数相加

---
+++ algorithm-leetcode/src/main/java/net/ityizhan/algorithm/medium/C0002_addTwoNumbers.java
@@ -0,0 +1,54 @@
+package net.ityizhan.algorithm.medium;
+
+/**
+ * @ClassName C0002_addTwoNumbers
+ * @Description: 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * @Author 骚豪 程序驿站: https://www.ityizhan.net
+ * @Date 2021/7/5 10:22
+ * @Version V1.0.0d
+ **/
+public class C0002_addTwoNumbers {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+
+        ListNode pre = new ListNode(0);
+        ListNode cur = pre;
+        while (l1 != null || l2 != null) {
+            int num1 = l1 != null ? l1.val : 0;
+            int num2 = l2 != null ? l2.val : 0;
+            int sum = num1 + num2 + carry;
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry == 1) {
+            cur.next = new ListNode(carry);
+        }
+        return pre.next;
+    }
+
+}
